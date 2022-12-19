a9a0a0397a7bc110c83b4e04d5bb5f1536e0b07f
Nikli Erik <h162267@stud.u-szeged.hu>
2022-12-19 16:48:02 +0100

    Tests on SyntaxUtils

---
+++ Tests/CodeAnalysis/Syntax/ParserTest.cs
@@ -0,0 +1,14 @@
+﻿namespace Theta.Tests.CodeAnalysis.Syntax;
+using System;
+using Theta.CodeAnalysis.Syntax;
+
+public class ParserTest
+{
+
+
+    //[Theory]
+   // public void Parser_BinaryExpression_HonorsPrecedences(SyntaxType op1, SyntaxType op2)
+   // {
+
+   // }
+}
---
+++ Tests/CodeAnalysis/SyntaxUtilsTest.cs
@@ -0,0 +1,45 @@
+﻿using Theta.CodeAnalysis;
+using Theta.CodeAnalysis.Syntax;
+
+namespace Theta.Tests.CodeAnalysis;
+
+public class SyntaxUtilsTest
+{
+    [Theory]
+    [MemberData(nameof(GetSyntaxTypes))]
+    public void SyntaxUtils_GetText_RoundTrips(SyntaxType type)
+    {
+        var text = type.GetSyntaxText();
+        if (text is null)
+        {
+            return;
+        }
+        var tokens = new Lexer(text);
+        var token = Assert.Single(tokens) as SyntaxToken;
+        Assert.NotNull(token);
+        Assert.Equal(type, token.Type);
+        Assert.Equal(text, token.Text);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetOperators))]
+    public void SyntaxUtils_GetOperatorPrecedence_IsPositive(SyntaxType operatorToken)
+    {
+        var precedence = operatorToken.GetUnaryOperatorPrecedence();
+        if (precedence <= 0)
+        {
+            precedence = operatorToken.GetBinaryOperatorPrecedence();
+        }
+        Assert.True(precedence > 0, "Precedence must be positive");
+    }
+
+    public static IEnumerable<object[]> GetSyntaxTypes()
+    {
+        return ((SyntaxType[]) Enum.GetValues(typeof(SyntaxType))).Select(value => new object[] { value });
+    }
+
+    public static IEnumerable<object[]> GetOperators()
+    {
+        return SyntaxUtils.OperatorTokens.Select(token => new object[] { token });
+    }
+}
