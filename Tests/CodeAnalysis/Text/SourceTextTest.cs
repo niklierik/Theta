35e93bf8d0caa8376613311498e1fc503edf5d6f
Nikli Erik <h162267@stud.u-szeged.hu>
2022-12-20 14:57:47 +0100

    Test for line numbers

---
+++ Tests/CodeAnalysis/Text/SourceTextTest.cs
@@ -0,0 +1,22 @@
+﻿namespace Theta.Tests.CodeAnalysis.Text;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Theta.CodeAnalysis.Text;
+
+public class SourceTextTest
+{
+    [Theory]
+    [InlineData(".", 1)]
+    [InlineData(".\r\n", 2)]
+    [InlineData(".\n", 2)]
+    [InlineData(".\r\n\r\n", 3)]
+    public void SourceText_IncludeLastLine(string text, int expectedLineCount)
+    {
+        var sourceText = SourceText.From(text);
+        Assert.Equal(expectedLineCount, sourceText.Lines.Count);
+    }
+}
